8b0086d2fe177c666669dd0792a5e4573c3ef9c7
xutongtong <xutongtong@yullion.com>
2017-12-22 16:15:16 +0800

    test orm drds

--- main.go
+++ main.go
@@ -1,9 +1,27 @@
 package main
 
 import (
-	"Users/apis"
+	//"Users/apis"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
+type Users struct {
+	gorm.Model
+	ID int64 `gorm:"primary_key"`
+	Name string
+	Password string
+}
+
 func main() {
-	apis.Init()
+	db, err := gorm.Open("mysql", "account:123456789aABC$@tcp(drds4ln637rupoos.drds.aliyuncs.com:3306)/account?charset=utf8bmp&parseTime=True&loc=Local")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
+
+	db.AutoMigrate(&Users{})
+
+	db.Create(Users{Name:"xutt",Password:"123456"})
+	//apis.Init()
 }
